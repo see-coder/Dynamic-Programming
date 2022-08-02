9726ef7a5b0f9554c20fa19e1992b61b90f8231a
see-coder <imrakesh.cse@gmail.com>
2022-08-02 19:21:34 +0530

    Memoization implemented in can-sum (wrong output)

--- 01-memoization/010-can-sum/main.py
+++ 01-memoization/010-can-sum/main.py
@@ -1,5 +1,7 @@
-# brute force solution
-def can_sum(target_sum, numbers):
+# memoized solution
+def can_sum(target_sum, numbers, memo = {}):
+    if target_sum in memo:
+        return memo[target_sum]
     if target_sum == 0:
         return True
     if target_sum < 0:
@@ -7,9 +9,11 @@ def can_sum(target_sum, numbers):
     
     for num in numbers:
         remainder = target_sum - num
-        if can_sum(remainder, numbers) == True:
+        if can_sum(remainder, numbers, memo) == True:
+            memo[target_sum] = True
             return True
     
+    memo[target_sum] = False
     return False
 
 # Testing
@@ -17,4 +21,4 @@ print(can_sum(7, [2, 3]))
 print(can_sum(7, [5, 3, 4, 7]))
 print(can_sum(7, [2, 4]))
 print(can_sum(8, [2, 3, 5]))
-print(can_sum(300, [7, 14])) # can_sum is not efficient for this data
+print(can_sum(300, [7, 14])) # Now can_sum is efficient for this data
