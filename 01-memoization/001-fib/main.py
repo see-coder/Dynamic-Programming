eae69afb45a3239e55958802c0d509fca06fd8d9
see-coder <imrakesh.cse@gmail.com>
2022-08-02 12:19:04 +0530

    Memoization implemented in fib

--- 01-memoization/001-fib/main.py
+++ 01-memoization/001-fib/main.py
@@ -1,11 +1,14 @@
-# brute force solution
-def fib(n):
+# memoized solution
+def fib(n, memo = {}):
+    if n in memo:
+        return memo[n]
     if n <= 2:
         return 1
-    return fib(n-1) + fib(n-2)
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
 
 # Testing
 print(fib(6))
 print(fib(7))
 print(fib(8))
-print(fib(50)) # fib is not efficient for this data
+print(fib(50)) # Now fib is efficient for this data
