590b6fc1a31b4b4ba374ea29699da760780e7692
see-coder <imrakesh.cse@gmail.com>
2022-08-03 16:46:21 +0530

    Memoization implemented in how-sum

--- 01-memoization/015-how-sum/main.py
+++ 01-memoization/015-how-sum/main.py
@@ -1,5 +1,9 @@
-# brute force solution
-def how_sum(target_sum, numbers):
+# memoized solution
+def how_sum(target_sum, numbers, memo = None):
+    if memo is None:
+        memo = {}
+    if target_sum in memo:
+        return memo[target_sum]
     if target_sum == 0:
         return []
     if target_sum < 0:
@@ -7,11 +11,13 @@ def how_sum(target_sum, numbers):
     
     for num in numbers:
         remainder = target_sum - num
-        remainder_result = how_sum(remainder, numbers)
+        remainder_result = how_sum(remainder, numbers, memo)
         if remainder_result != None:
             remainder_result.append(num)
+            memo[target_sum] = remainder_result
             return remainder_result
-            
+
+    memo[target_sum] = None
     return None
 
 # Testing
@@ -19,4 +25,4 @@ print(how_sum(7, [2, 3]))
 print(how_sum(7, [5, 3, 4, 7]))
 print(how_sum(7, [2, 4]))
 print(how_sum(8, [2, 3, 5]))
-print(how_sum(300, [7, 14])) # how_sum is not efficient for this data
+print(how_sum(300, [7, 14])) # Now how_sum is efficient for this data
