7b84df119ae9b44a7042c863f0707d6434e166c6
see-coder <imrakesh.cse@gmail.com>
2022-08-04 11:06:53 +0530

    Memoization implemented in best-sum

--- 01-memoization/020-best-sum/main.py
+++ 01-memoization/020-best-sum/main.py
@@ -1,5 +1,9 @@
-# brute force solution
-def best_sum(target_sum, numbers):
+# memoized solution
+def best_sum(target_sum, numbers, memo = None):
+    if memo is None:
+        memo = {}
+    if target_sum in memo:
+        return memo[target_sum]
     if target_sum == 0:
         return []
     if target_sum < 0:
@@ -9,17 +13,18 @@ def best_sum(target_sum, numbers):
     
     for num in numbers:
         remainder = target_sum - num
-        remainder_combination = best_sum(remainder, numbers)
+        remainder_combination = best_sum(remainder, numbers, memo)
         if remainder_combination != None:
             combination = remainder_combination.copy()
             combination.append(num)
             if shortest_combination == None or len(combination) < len(shortest_combination):
                 shortest_combination = combination
             
+    memo[target_sum] = shortest_combination
     return shortest_combination
 
 # Testing
 print(best_sum(7, [5, 3, 4, 7]))
 print(best_sum(8, [2, 3, 5]))
 print(best_sum(8, [1, 4, 5]))
-print(best_sum(100, [1, 2, 5, 25])) # best_sum is not efficient for this data
+print(best_sum(100, [1, 2, 5, 25])) # Now best_sum is efficient for this data
