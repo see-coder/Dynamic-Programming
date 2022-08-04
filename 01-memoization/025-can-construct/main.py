bba28b47b59d5219c94bf94084dd5899c54e1003
see-coder <imrakesh.cse@gmail.com>
2022-08-04 14:30:10 +0530

    Memoization implemented in can-construct

--- 01-memoization/025-can-construct/main.py
+++ 01-memoization/025-can-construct/main.py
@@ -1,19 +1,25 @@
-# brute force solution
-def can_construct(target, word_bank):
+# memoized solution
+def can_construct(target, word_bank, memo = None):
+    if memo is None:
+        memo = {}
+    if target in memo:
+        return memo[target]
     if target == '':
         return True
     
     for word in word_bank:
         if target.startswith(word):
             suffix = target[len(word):]
-            if can_construct(suffix, word_bank) == True:
+            if can_construct(suffix, word_bank, memo) == True:
+                memo[target] = True
                 return True
     
+    memo[target] = False
     return False
 
 # Testing
 print(can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
 print(can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
 print(can_construct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
-# can_construct is not efficient for this data
+# Now can_construct is efficient for this data
 print(can_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]))
